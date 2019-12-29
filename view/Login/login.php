79fa03021655781747e92a3b7527826d35a79846
sovonthy <sovonthy.leng069@gmail.com>
2019-12-29 13:31:55 +0700

    login form

---
+++ view/Login/login.php
@@ -0,0 +1,19 @@
+
+<form action="index.php?action=homepage" method="POST">
+  <div class="card">
+    <div class="card-header">
+       <h3>Login form</h3>
+    </div>
+    <div class="card-body">
+        <div class="form-group">
+        <label for="">User</label>
+          <input type="text" name="user">
+        </div>
+        <div class="form-group">
+        <label for="">Pass</label>
+          <input type="password" name="pass">
+        </div>
+        <button type="submit" name="login">Login</button>
+    </div>
+  </div>
+  </form>
